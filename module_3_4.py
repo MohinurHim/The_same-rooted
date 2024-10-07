cf5c4ebe86190bd204e09f0a1f310da215c50bca
MohinurHim <himmatovamohinur@gmail.com>
2024-10-07 11:04:52 +0300

    Update module_3_4.py
    Произвольное число параметров. Однокоренные

--- module_3_4.py
+++ module_3_4.py
@@ -1,13 +1,14 @@
 # Однокоренные
 def single_root_words(root_word, *other_words):
     same_words = []
-    for word in other_words:
-        if root_word in word or word in root_word:
-            same_words.append(word)
+    word = root_word.lower()
+    for w in other_words:
+        word2 = w.lower()
+        if word in word2 or word2 in word:
+            same_words.append(w)
     return same_words
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result1)
 print(result2)
-
